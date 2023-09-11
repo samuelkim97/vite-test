ad1fc6d556f5b775a040c6ebbf1c52a3835a4d48
Bumsoo Kim <kbeom00@gmail.com>
2023-09-11 21:17:37 +0900

    done props part

--- src/index.js
+++ src/index.js
@@ -2,25 +2,47 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const books = [
+  {
+    author: "Walter Isaacson",
+    title: "Elon Musk",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/814mI0-rkxL._AC_UL600_SR600,400_.jpg",
+    id: 1,
+  },
+  {
+    author: "Stephen King",
+    title: "Holly",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/815oQ6G6HDL._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+  {
+    author: "Rebecca Yarros",
+    title: "Iron Flame",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/91ke43dIxkL._AC_UL600_SR600,400_.jpg",
+    id: 3,
+  },
+];
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        const { image, author, title } = book;
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 };
 
-const Book = () => {
-  const author = "Walter Isaacson";
+const Book = (props) => {
+  const { image, title, author } = props;
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/814mI0-rkxL._AC_UL600_SR600,400_.jpg"
-        alt="Elon Musk"
-      />
-      <h2>Elon Musk</h2>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
       <h4>{author}</h4>
     </article>
   );
