ccc81d49d42f4f82ffa8ff5f8d6b81d8f3915533
Bumsoo Kim <kbeom00@gmail.com>
2023-09-12 22:30:52 +0900

    created images file

--- src/books.js
+++ src/books.js
@@ -1,23 +1,24 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
 export const books = [
   {
     author: "Walter Isaacson",
     title: "Elon Musk",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/814mI0-rkxL._AC_UL600_SR600,400_.jpg",
+    image: img1,
     id: 1,
   },
   {
     author: "Stephen King",
     title: "Holly",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/815oQ6G6HDL._AC_UL600_SR600,400_.jpg",
+    image: img2,
     id: 2,
   },
   {
     author: "Rebecca Yarros",
     title: "Iron Flame",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/91ke43dIxkL._AC_UL600_SR600,400_.jpg",
+    image: img3,
     id: 3,
   },
 ];
